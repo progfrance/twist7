27fcaef7973f9f179050e97ec3661115d3e2c883
progfrance <progfrance@gmail.com>
2026-08-05 15:56:33 +0200

    build: silence benign react-refresh warnings (rule off)

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
     rules: {
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
